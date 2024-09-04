7cdd1fd04592342f03705a3b2e5b9d49659ac879
Andarch <162850255+Andarch@users.noreply.github.com>
2024-09-04 02:40:49 -0500

    Successfully moved open/save functions

--- H3_HotA_MEX.py
+++ H3_HotA_MEX.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-import ctypes
 import sys
-import threading
-from enum import Enum
-from gzip import open
+# from enum import Enum
 from src import *
 
 HELP_COMMANDS = ["help", "h"]
@@ -41,135 +38,6 @@ def main() -> None:
     ####################
     # HELPER FUNCTIONS #
     ####################
-
-    def open_maps() -> None:
-        def main() -> str:
-            while True:
-                start_new_screen()
-                xprint(type = MSG.MENU, menu_item = 1, text = "Open 1 map")
-                xprint(type = MSG.MENU, menu_item = 2, text = "Open 2 maps")
-                result = process_key_press(detect_key_press("12"))
-                if result == "esc":
-                    break
-        def process_key_press(choice: str) -> str:
-            def main() -> str:
-                match choice:
-                    case "1":
-                        map_data["Map 1"] = {}
-                        map_data["Map 2"] = None
-                    case "2":
-                        map_data["Map 1"] = {}
-                        map_data["Map 2"] = {}
-                    case "esc":
-                        return "esc"
-                while True:
-                    start_new_screen()
-                    result = process_filename("Map 1", choice)
-                    match result:
-                        case "success": return "success"
-                        case "failure": continue
-                        case "esc": return ""
-                    if choice == "2":
-                        if not process_filename("Map 2", choice):
-                            continue
-            def process_filename(map_key: str, choice: str) -> str:
-                match choice:
-                    case "1":
-                        prompt = "Enter the map filename"
-                    case "2":
-                        prompt = f"Enter the filename for {map_key}"
-                filename = xprint(type = MSG.PROMPT, text = prompt)
-                if filename:
-                    filename = append_h3m(filename)
-                    if open_map(filename, map_key):
-                        return "success"
-                    else:
-                        xprint(type = MSG.ERROR, text = f"Could not find {filename}.", flush = True)
-                        return "failure"
-                else:
-                    return "esc"
-            return main()
-        return main()
-
-    def open_map(filename: str, map_key: str) -> None:
-        xprint(type = MSG.ACTION, text = f"Loading {filename}...")
-        try:
-            with open(filename, "rb"):
-                pass
-        except FileNotFoundError:
-            return False
-        with open(filename, "rb") as io.in_file:
-            map_data[map_key]["filename"]     = filename
-            map_data[map_key]["general"]      = h1.parse_general()
-            map_data[map_key]["player_specs"] = h2.parse_player_specs()
-            map_data[map_key]["conditions"]   = h3.parse_conditions()
-            map_data[map_key]["teams"]        = h2.parse_teams()
-            map_data[map_key]["start_heroes"] = h4.parse_starting_heroes(map_data[map_key]["general"])
-            map_data[map_key]["ban_flags"]    = h5.parse_flags()
-            map_data[map_key]["rumors"]       = h6.parse_rumors()
-            map_data[map_key]["hero_data"]    = h4.parse_hero_data()
-            map_data[map_key]["terrain"]      = h7.parse_terrain(map_data[map_key]["general"])
-            map_data[map_key]["object_defs"]  = h8.parse_object_defs()
-            map_data[map_key]["object_data"]  = h8.parse_object_data(map_data[map_key]["object_defs"])
-            map_data[map_key]["events"]       = h6.parse_events()
-            map_data[map_key]["null_bytes"]   = io.in_file.read()
-        xprint(type = MSG.SPECIAL, text = DONE)
-        terminal_width = get_terminal_width()
-        xprint(type = MSG.INFO, text = "-" * terminal_width)
-        xprint(type = MSG.INFO, text = f"{map_data[map_key]["general"]["name"]}")
-        xprint(type = MSG.INFO, text = "-" * terminal_width)
-        xprint(type = MSG.INFO, text = f"{map_data[map_key]["general"]["description"]}")
-        xprint(type = MSG.INFO, text = "-" * terminal_width)
-        return True
-
-    # def save_maps() -> None:
-    #     if map_data["Map 2"] is not None:
-    #         filename1 = print_string_prompt("Enter a filename to save Map 1")
-    #         if filename1.lower() in BACK_COMMANDS:
-    #             print(ABORT_MSG)
-    #             return
-    #         save_map(filename1, "Map 1")
-    #         filename2 = print_string_prompt("Enter a filename to save Map 2")
-    #         if filename2.lower() in BACK_COMMANDS:
-    #             print(ABORT_MSG)
-    #             return
-    #         save_map(filename2, "Map 2")
-    #     else:
-    #         filename1 = print_string_prompt("Enter a filename to save the map")
-    #         if filename1.lower() in BACK_COMMANDS:
-    #             print(ABORT_MSG)
-    #             return
-    #         save_map(filename1, "Map 1")
-
-    # def save_map(filename: str, map_key: str) -> None:
-    #     if len(filename) > 4:
-    #         if filename[-4:] != ".h3m":
-    #             filename += ".h3m"
-    #     else: filename += ".h3m"
-
-    #     print_action("Saving map...")
-
-    #     with open(filename, "wb") as io.out_file:
-    #         h1.write_general(        map_data[map_key]["general"])
-    #         h2.write_player_specs(   map_data[map_key]["player_specs"])
-    #         h3.write_conditions(     map_data[map_key]["conditions"])
-    #         h2.write_teams(          map_data[map_key]["teams"])
-    #         h4.write_starting_heroes(map_data[map_key]["start_heroes"])
-    #         h5.write_flags(          map_data[map_key]["ban_flags"])
-    #         h6.write_rumors(         map_data[map_key]["rumors"])
-    #         h4.write_hero_data(      map_data[map_key]["hero_data"])
-    #         h7.write_terrain(        map_data[map_key]["terrain"])
-    #         h8.write_object_defs(    map_data[map_key]["object_defs"])
-    #         h8.write_object_data(    map_data[map_key]["object_data"])
-    #         h6.write_events(         map_data[map_key]["events"])
-    #         io.out_file.write(       map_data[map_key]["null_bytes"])
-
-    #     print_done()
-
-    def append_h3m(filename: str) -> str:
-        if filename[-4:] != ".h3m":
-            filename += ".h3m"
-        return filename
 
     # def get_map_key() -> str:
     #     if map_data["Map 2"] is None:
@@ -208,7 +76,7 @@ def main() -> None:
                     xprint(type = MSG.MENU, menu_item = i + 1, text = option)
                 choice = detect_key_press("12")
                 match choice:
-                    case "1": open_maps()
+                    case "1": io.open_map_prompts()
                     case "2": exit()
                 time.sleep(SLEEP.TIC)
         except KeyboardInterrupt:
@@ -230,7 +98,7 @@ def main() -> None:
     #     try:
     #         match command.split():
     #             case ["load"] | ["open"]:
-    #                 open_maps()
+    #                 open_map_prompts()
 
     #             case ["save"]:
     #                 if map_data["Map 1"] is None:
