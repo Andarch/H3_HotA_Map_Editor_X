8892f75cabf579d23c13331a6f6c4c8d9854cfd3
Andarch <andarchitect1@gmail.com>
2024-11-29 12:45:21 -0600

    Minor update

--- src/scripts/export_json.py
+++ src/scripts/export_json.py
@@ -7,18 +7,30 @@ class CustomEncoder(json.JSONEncoder):
             return obj.decode('latin-1')
         return json.JSONEncoder.default(self, obj)
 
-def export_json(map_key: dict, filename: str) -> None:
-    if filename.endswith('.h3m'):
-        filename = filename[:-4]
-    if not filename.endswith('.json'):
-        filename += '.json'
-    
-    xprint(type=Text.ACTION, text=f"Exporting JSON file...")
+def export_json(map_key: dict, filename: str) -> bool:
+    def main(map_key: dict, filename: str) -> bool:
+        if filename.endswith('.h3m'):
+            filename = filename[:-4]
+        if not filename.endswith('.json'):
+            filename += '.json'
 
-    try:
-        with open(filename, 'w') as f:
-            json.dump(map_key, f, cls = CustomEncoder, indent = 4)
-        xprint(type=Text.SPECIAL, text=DONE)
-    except Exception as e:
-        xprint(type=Text.ERROR, text=f"Failed to export JSON file: {e}")
+        #while True:
+        type = get_export_type()
+        if not type: return False
+        match type:
+            case 1: data = map_key
+            case 2: data = map_key['terrain']
         
+        xprint(type=Text.ACTION, text=f"Exporting JSON file...")
+
+        with open(filename, 'w') as f:
+            json.dump(data, f, cls = CustomEncoder, indent = 4)
+        xprint(type=Text.SPECIAL, text=DONE)
+        return True
+
+    def get_export_type() -> int:
+        input = xprint(menu=Menu.JSON.value)
+        if input == ESC: return False
+        else: return int(input)
+
+    return main(map_key, filename)
