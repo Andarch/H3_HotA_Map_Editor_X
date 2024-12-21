7561adc4127e3d12eeb420e0c7a1c82179132d55
Andarch <andarchitect1@gmail.com>
2024-12-21 02:35:04 -0600

    Updated init

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -3,6 +3,5 @@ from .export_json      import export_json
 from .generate_minimap import generate_minimap
 from .modify_towns     import modify_towns
 from .print_data       import print_data
-from .scan_zones       import scan_zones
 from .swap_layers      import swap_layers
 from .update_events    import update_events
