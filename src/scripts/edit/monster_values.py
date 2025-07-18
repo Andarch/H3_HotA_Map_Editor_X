3ebe1e5823f6149f9d238f86a61528364eba4a04
Andarch <andarchitect1@gmail.com>
2025-07-18 14:43:03 -0500

    Monster values

--- src/scripts/edit/monster_values.py
+++ src/scripts/edit/monster_values.py
@@ -1,4 +1,5 @@
 import data.objects as objects
+import math
 from ...common import *
 
 def set_compliant_monster_values(obj_data: list) -> bool:
@@ -11,9 +12,18 @@ def set_compliant_monster_values(obj_data: list) -> bool:
             obj.get("zone_type") == "Player" and
             obj.get("disposition") == 0):  # Disposition.Compliant = 0
 
-            # Multiply ai_value by 3
-            old_value = obj["ai_value"]
-            obj["ai_value"] = old_value * 3
+            match obj["ai_value"]:
+                case 1000:
+                    obj["ai_value"] = 3000
+                case 1500:
+                    obj["ai_value"] = 10000
+                case 2000:
+                    obj["ai_value"] = 15000
+                case 2500:
+                    obj["ai_value"] = 25000
+                case 10000:
+                    obj["ai_value"] = 100000
+
             count += 1
 
     xprint(type=Text.SPECIAL, text=DONE)
