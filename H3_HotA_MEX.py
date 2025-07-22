7ff3271cc4195483d75dfc22c8887e724ab01fdc
Andarch <andarchitect1@gmail.com>
2025-07-22 02:03:15 -0500

    Updates

--- H3_HotA_MEX.py
+++ H3_HotA_MEX.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-from src import *
 
+from src import *
 
 os.chdir("maps")
 
 
 def main() -> None:
-    if(initialize()):
+    if initialize():
         try:
             io.load_map()
 
@@ -22,22 +22,32 @@ def main() -> None:
                 # xprint()
 
                 match user_input:
-                    case 1: scripts.print_data()
-                    case 2: scripts.edit_data()
-                    case 3: scripts.export_excel(map_data)
-                    case 4: scripts.export_json(map_data)
-                    case 5: scripts.generate_minimap(
-                                map_data["filename"],
-                                map_data["map_specs"],
-                                map_data["terrain"],
-                                map_data["object_data"],
-                                map_data["object_defs"]
-                            )
-                    case 6: io.load_map(quickload=True)
-                    case 7: io.save_map(quicksave=True)
-                    case 8: io.load_map()
-                    case 9: io.save_map()
-                    case 0: exit()
+                    case 1:
+                        scripts.print_data()
+                    case 2:
+                        scripts.edit_data()
+                    case 3:
+                        scripts.export_excel(map_data)
+                    case 4:
+                        scripts.export_json(map_data)
+                    case 5:
+                        scripts.generate_minimap(
+                            map_data["filename"],
+                            map_data["map_specs"],
+                            map_data["terrain"],
+                            map_data["object_data"],
+                            map_data["object_defs"],
+                        )
+                    case 6:
+                        io.load_map(quickload=True)
+                    case 7:
+                        io.save_map(quicksave=True)
+                    case 8:
+                        io.load_map()
+                    case 9:
+                        io.save_map()
+                    case 0:
+                        exit()
                 time.sleep(Sleep.TIC.value)
         except KeyboardInterrupt:
             exit()
--- src/common.py
+++ src/common.py
@@ -24,65 +24,65 @@ class KB(Enum):
 
 
 class Align(Enum):
-    LEFT   = 1
+    LEFT = 1
     CENTER = 2
-    MENU   = 3
-    FLUSH  = 4
+    MENU = 3
+    FLUSH = 4
 
 
 class Color(Enum):
-    RESET     = "\033[0m"
-    FAINT     = "\033[2m"
-    ITALIC    = "\033[3m"
+    RESET = "\033[0m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
     UNDERLINE = "\033[4m"
-    BLINK     = "\033[5m"
-    INVERTED  = "\033[7m"
-    STRIKE    = "\033[9m"
-    MAGENTA2  = "\033[35m"
-    DEFAULT   = "\033[39m"
-    RED       = "\033[91m"
-    GREEN     = "\033[92m"
-    YELLOW    = "\033[93m"
-    BLUE      = "\033[94m"
-    MAGENTA1  = "\033[95m"
-    CYAN      = "\033[96m"
-    WHITE     = "\033[97m"
-    GREY1     = "\033[90m"
-    GREY2     = "\033[90;2m"
+    BLINK = "\033[5m"
+    INVERTED = "\033[7m"
+    STRIKE = "\033[9m"
+    MAGENTA2 = "\033[35m"
+    DEFAULT = "\033[39m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    MAGENTA1 = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    GREY1 = "\033[90m"
+    GREY2 = "\033[90;2m"
 
 
 class Sleep(Enum):
-    TIC    = 0.01
-    SHORTER  = 0.05
+    TIC = 0.01
+    SHORTER = 0.05
     SHORT = 0.25
     NORMAL = 0.75
-    LONG   = 1.5
+    LONG = 1.5
 
 
 class Text(Enum):
-    NORMAL  = 1
-    INFO    = 2
-    MENU    = 3
-    PROMPT  = 4
-    ACTION  = 5
+    NORMAL = 1
+    INFO = 2
+    MENU = 3
+    PROMPT = 4
+    ACTION = 5
     SPECIAL = 6
-    HEADER  = 7
-    ERROR   = 8
+    HEADER = 7
+    ERROR = 8
 
 
 map_data = {}
 
-_terminal_width     = 0
+_terminal_width = 0
 _old_terminal_width = 0
-_redrawing_screen   = False
-_screen_cache       = []
+_redrawing_screen = False
+_screen_cache = []
 
 
 def initialize():
     global _terminal_width, _old_terminal_width
 
     HIDE_CURSOR = "\033[?25l"
-    print(HIDE_CURSOR, end = "", flush = True)
+    print(HIDE_CURSOR, end="", flush=True)
 
     _terminal_width = _old_terminal_width = shutil.get_terminal_size().columns
 
@@ -95,7 +95,7 @@ def initialize():
         xprint(type=Text.ERROR, text="Another instance of the editor is already running.")
         return False
 
-    monitor_thread1 = threading.Thread(target = monitor_terminal_size, daemon = True)
+    monitor_thread1 = threading.Thread(target=monitor_terminal_size, daemon=True)
     monitor_thread1.start()
 
     return True
@@ -189,10 +189,20 @@ def fill_header_row(fill_color: str, fill: str, text: str = "") -> str:
     return row
 
 
-def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=False, menu_num=-1, menu_width=0, menu={}) -> Union[None, Union[int, str]]:
+def xprint(
+    type=Text.NORMAL,
+    text="",
+    align=Align.LEFT,
+    overwrite=0,
+    skipline=False,
+    menu_num=-1,
+    menu_width=0,
+    menu={},
+) -> Union[None, Union[int, str]]:
     def main() -> Union[None, Union[int, str]]:
         global _screen_cache
-        if menu: return menu_prompt(menu)
+        if menu:
+            return menu_prompt(menu)
         if not _redrawing_screen and type != Text.HEADER:
             _screen_cache.append((type, text, align, menu_num, menu_width))
         match type:
@@ -205,11 +215,13 @@ def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=Fa
             case Text.INFO:
                 print(align_text(text=f"{Color.CYAN.value}{text}{Color.RESET.value}"))
             case Text.MENU:
-                print(align_text(
-                    align=Align.MENU,
-                    text=f"[{Color.YELLOW.value}{str(menu_num)}{Color.RESET.value}] {Color.WHITE.value}{text}{Color.RESET.value}",
-                    menu_width=menu_width
-                ))
+                print(
+                    align_text(
+                        align=Align.MENU,
+                        text=f"[{Color.YELLOW.value}{str(menu_num)}{Color.RESET.value}] {Color.WHITE.value}{text}{Color.RESET.value}",
+                        menu_width=menu_width,
+                    )
+                )
             case Text.PROMPT:
                 xprint()
                 input = string_prompt(align_text(text=f"{Color.YELLOW.value}[{text}] > {Color.WHITE.value}"))
@@ -219,7 +231,11 @@ def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=Fa
                     time.sleep(Sleep.SHORTER.value)
                     for _ in range(overwrite):
                         print("\033[F\033[K", end="")
-                print(align_text(text=f"{Color.WHITE.value}{text}{Color.RESET.value}"), end = " ", flush = True)
+                print(
+                    align_text(text=f"{Color.WHITE.value}{text}{Color.RESET.value}"),
+                    end=" ",
+                    flush=True,
+                )
                 time.sleep(Sleep.NORMAL.value)
             case Text.SPECIAL:
                 print(f"{Color.GREEN.value}{text}{Color.RESET.value}")
@@ -266,6 +282,7 @@ def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=Fa
             valid_keys = print_menu(menu, width)
             input = detect_key_press(valid_keys)
             return input
+
         def get_menu_width(menu: dict) -> int:
             width = 0
             for _, text in menu.items():
@@ -274,21 +291,18 @@ def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=Fa
                 if text_length > width:
                     width = text_length
             return width
+
         def print_menu(menu: dict, width: int) -> str:
             valid_keys = ""
             for key, value in menu.items():
                 if value:
                     valid_keys += str(key)
-                    xprint(
-                        type=Text.MENU,
-                        text=value,
-                        menu_num=key,
-                        menu_width=width
-                    )
+                    xprint(type=Text.MENU, text=value, menu_num=key, menu_width=width)
                 else:
                     xprint()
             xprint()
             return valid_keys
+
         def detect_key_press(valid_keys: str) -> str:
             while True:
                 char = msvcrt.getwch()
@@ -296,10 +310,11 @@ def xprint(type=Text.NORMAL, text="", align=Align.LEFT, overwrite=0, skipline=Fa
                     return int(char)
                 elif ord(char) == KB.ESC.value:
                     return KB.ESC.value
+
         return main()
 
     def string_prompt(prompt: str) -> str:
-        print(prompt, end = "", flush = True)
+        print(prompt, end="", flush=True)
         input_chars = []
         while True:
             char = msvcrt.getwch()
