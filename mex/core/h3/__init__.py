73847c4d4177e1bbe1f303e9d02465e03be8f87e
Andarch <andarchitect1@gmail.com>
2025-08-20 21:31:19 -0500

    Fix

--- mex/core/h3/__init__.py
+++ mex/core/h3/__init__.py
@@ -1,1 +1,7 @@
+from . import artifacts as artifacts
+from . import creatures as creatures
+from . import heroes as heroes
 from . import objects as objects
+from . import players as players
+from . import skills as skills
+from . import spells as spells
