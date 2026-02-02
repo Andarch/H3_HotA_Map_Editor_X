fff220417415bf919844bce95e8e7c6b4d1fe214
Andarch <162850255+Andarch@users.noreply.github.com>
2026-02-01 20:45:17 -0600

    map

--- h3mex/src/export/export.py
+++ h3mex/src/export/export.py
@@ -25,56 +25,57 @@ OBJECT_FILTER = [*objects.ID]
 
 
 def menu() -> None:
-    keypress = xprint(menu=(Menu.EXPORT["name"], Menu.EXPORT["menus"][0]))
-    if keypress == Keypress.ESC:
-        return
+    while True:
+        keypress = xprint(menu=(Menu.EXPORT["name"], Menu.EXPORT["menus"][0]))
+        if keypress == Keypress.ESC:
+            return
 
-    header.draw()
+        header.draw()
 
-    match keypress:
-        case "1":
-            minimap.generate(minimap.MMAction.EXPORT, minimap.MMType.STANDARD, None)
-        case "2":
-            minimap.generate(minimap.MMAction.EXPORT, minimap.MMType.EXTENDED, None)
-        case "3":
-            while True:
-                keypress = xprint(menu=(Menu.EXPORT["name"], Menu.EXPORT["menus"][1]))
-                if keypress == Keypress.ESC:
-                    break
-                match keypress:
-                    case "1":
-                        data = map_data["general"]
-                    case "2":
-                        data = map_data["player_specs"]
-                    case "3":
-                        data = map_data["starting_heroes"]
-                    case "4":
-                        data = map_data["ban_flags"]
-                    case "5":
-                        data = map_data["rumors"]
-                    case "6":
-                        data = map_data["hero_data"]
-                    case "7":
-                        data = map_data["terrain"]
-                    case "8":
-                        data = map_data["object_defs"]
-                    case "9":
-                        data = [
-                            obj
-                            for obj in map_data["object_data"]
-                            if obj["id"] in OBJECT_FILTER
-                            # if obj["id"] in OBJECT_FILTER and obj["sub_id"] in OBJECT_SUBFILTER
-                            # if obj["id"] in OBJECT_FILTER and obj["coords"] in OBJECT_COORDS_FILTER
-                        ]
-                    case "0":
-                        data = map_data["town_events"]
-                    case "E":
-                        data = map_data["global_events"]
-                filepath = "exports/json"
-                filename = os.path.join(filepath, map_data["filename"][:-4] + ".json")
-                xprint(type=TextType.ACTION, text="Exporting JSON file…", overwrite=14)
-                with open(filename, "w") as f:
-                    json.dump(data, f, cls=CustomEncoder, indent=4)
-                xprint(type=TextType.DONE)
+        match keypress:
+            case "1":
+                minimap.generate(minimap.MMAction.EXPORT, minimap.MMType.STANDARD, None)
+            case "2":
+                minimap.generate(minimap.MMAction.EXPORT, minimap.MMType.EXTENDED, None)
+            case "3":
+                while True:
+                    keypress = xprint(menu=(Menu.EXPORT["name"], Menu.EXPORT["menus"][1]))
+                    if keypress == Keypress.ESC:
+                        break
+                    match keypress:
+                        case "1":
+                            data = map_data["general"]
+                        case "2":
+                            data = map_data["player_specs"]
+                        case "3":
+                            data = map_data["starting_heroes"]
+                        case "4":
+                            data = map_data["ban_flags"]
+                        case "5":
+                            data = map_data["rumors"]
+                        case "6":
+                            data = map_data["hero_data"]
+                        case "7":
+                            data = map_data["terrain"]
+                        case "8":
+                            data = map_data["object_defs"]
+                        case "9":
+                            data = [
+                                obj
+                                for obj in map_data["object_data"]
+                                if obj["id"] in OBJECT_FILTER
+                                # if obj["id"] in OBJECT_FILTER and obj["sub_id"] in OBJECT_SUBFILTER
+                                # if obj["id"] in OBJECT_FILTER and obj["coords"] in OBJECT_COORDS_FILTER
+                            ]
+                        case "0":
+                            data = map_data["town_events"]
+                        case "E":
+                            data = map_data["global_events"]
+                    filepath = "exports/json"
+                    filename = os.path.join(filepath, map_data["filename"][:-4] + ".json")
+                    xprint(type=TextType.ACTION, text="Exporting JSON file…", overwrite=14)
+                    with open(filename, "w") as f:
+                        json.dump(data, f, cls=CustomEncoder, indent=4)
+                    xprint(type=TextType.DONE)
 
-    xprint()
+        # xprint()
