2f150eda6a04d80ab88734954870740850bd6ab0
Andarch <162850255+Andarch@users.noreply.github.com>
2026-02-27 08:31:27 -0600

    Again

--- h3mex/src/ui/xprint.py
+++ h3mex/src/ui/xprint.py
@@ -127,30 +127,3 @@ def _align_text(align=TextAlign.LEFT, text="", menu_width=0) -> str:
     elif align == TextAlign.MENU:
         padding = (ui.terminal_width - menu_width) // 2
         return " " * padding + str(text)
-
-
-def _display_string_input_prompt(prompt: str) -> str:
-    print(prompt, end="", flush=True)
-    print(Cursor.SHOW, end="", flush=True)
-    chars = []
-    while True:
-        keypress = msvcrt.getwch()
-        match keypress:
-            case Keypress.ENTER:
-                if chars:
-                    print(Cursor.HIDE, end="", flush=True)
-                    xprint()
-                    xprint()
-                    return "".join(chars)
-                continue
-            case Keypress.BACKSPACE:
-                if chars:
-                    chars.pop()
-                    print("\b \b", end="", flush=True)
-                continue
-            case Keypress.ESC:
-                print(Cursor.HIDE, end="", flush=True)
-                return ""
-            case _:
-                chars.append(keypress)
-                print(keypress, end="", flush=True)
