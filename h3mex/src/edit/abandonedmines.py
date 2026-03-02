c58033b074019710fdc7b08e07081bc9bc703da3
Andarch <162850255+Andarch@users.noreply.github.com>
2026-03-02 00:13:51 -0600

    Fix

--- h3mex/src/edit/abandonedmines.py
+++ h3mex/src/edit/abandonedmines.py
@@ -25,42 +25,44 @@ def modify_abandoned_mines():
             and obj["sub_id"] == objects.SubID.Resource.Abandoned
             or obj["id"] == objects.ID.Abandoned_Mine
         ):
-            # Set resources
-            obj["resources"] = [0, 1, 0, 1, 1, 1, 1, 0]
+            obj["is_custom"] = True
 
-            # Set guards
-            if obj["zone_owner"] != players.ID.Neutral:
-                obj["creature"] = random.choice(list(creatures.Level5))
-                obj["min_val"] = 250
-                obj["max_val"] = 500
-            elif obj["zone_type"] == "I":
-                obj["creature"] = random.choice(list(creatures.Level1))
-                obj["min_val"] = 1000
-                obj["max_val"] = 1500
-            elif obj["zone_type"] == "II":
-                level = random.choice([2, 3, 4])
-                if level == 2:
-                    obj["creature"] = random.choice(list(creatures.Level2))
-                    obj["min_val"] = 1250
-                    obj["max_val"] = 1500
-                elif level == 3:
-                    obj["creature"] = random.choice(list(creatures.Level3))
-                    obj["min_val"] = 750
-                    obj["max_val"] = 1000
-                elif level == 4:
-                    obj["creature"] = random.choice(list(creatures.Level4))
-                    obj["min_val"] = 250
-                    obj["max_val"] = 500
-            elif obj["zone_type"] == "III":
-                level = random.choice([6, 7])
-                if level == 6:
-                    obj["creature"] = random.choice(list(creatures.Level6))
-                    obj["min_val"] = 500
-                    obj["max_val"] = 750
-                elif level == 7:
-                    obj["creature"] = random.choice(list(creatures.Level7))
-                    obj["min_val"] = 100
-                    obj["max_val"] = 250
+            # # Set resources
+            # obj["resources"] = [0, 1, 0, 1, 1, 1, 1, 0]
+
+            # # Set guards
+            # if obj["zone_owner"] != players.ID.Neutral:
+            #     obj["creature"] = random.choice(list(creatures.Level5))
+            #     obj["min_val"] = 250
+            #     obj["max_val"] = 500
+            # elif obj["zone_type"] == "I":
+            #     obj["creature"] = random.choice(list(creatures.Level1))
+            #     obj["min_val"] = 1000
+            #     obj["max_val"] = 1500
+            # elif obj["zone_type"] == "II":
+            #     level = random.choice([2, 3, 4])
+            #     if level == 2:
+            #         obj["creature"] = random.choice(list(creatures.Level2))
+            #         obj["min_val"] = 1250
+            #         obj["max_val"] = 1500
+            #     elif level == 3:
+            #         obj["creature"] = random.choice(list(creatures.Level3))
+            #         obj["min_val"] = 750
+            #         obj["max_val"] = 1000
+            #     elif level == 4:
+            #         obj["creature"] = random.choice(list(creatures.Level4))
+            #         obj["min_val"] = 250
+            #         obj["max_val"] = 500
+            # elif obj["zone_type"] == "III":
+            #     level = random.choice([6, 7])
+            #     if level == 6:
+            #         obj["creature"] = random.choice(list(creatures.Level6))
+            #         obj["min_val"] = 500
+            #         obj["max_val"] = 750
+            #     elif level == 7:
+            #         obj["creature"] = random.choice(list(creatures.Level7))
+            #         obj["min_val"] = 100
+            #         obj["max_val"] = 250
 
             modified_count += 1
 
